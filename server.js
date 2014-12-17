118bb3409ed83edcf5ca5f4843898027da0e258e
Guido García <guido@tid.es>
2014-12-17 20:43:43 +0100

    stringify

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.json(process.env);
+    res.end(JSON.stringify(process.env));
 }).listen(process.env.PORT0); // dynamic binding
 
 console.log('flock-demo running at http://0.0.0.0:%d/', process.env.PORT0);
