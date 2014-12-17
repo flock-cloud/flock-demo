0d5c52ea0903e7c46d7ce0a5c2fa35ecaff92013
Guido García <guido@tid.es>
2014-12-17 20:38:35 +0100

    json env + health checks

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Flock');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(process.env);
 }).listen(process.env.PORT0); // dynamic binding
 
 console.log('flock-demo running at http://0.0.0.0:%d/', process.env.PORT0);
