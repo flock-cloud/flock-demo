3cb8deea7bcbbf4d6e00e8feaa6ca13f721e6e23
Guido García <guido@tid.es>
2014-12-20 11:24:38 +0100

    log request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    console.log('new request received');
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(process.env));
 }).listen(process.env.PORT0); // dynamic binding
