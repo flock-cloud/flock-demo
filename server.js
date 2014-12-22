b3b9474355b2f31c27310a35d3f26f0293274bbe
Guido García <guido@tid.es>
2014-12-22 11:14:17 +0100

    add headers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    console.log('new request received');
+    console.log('new request received %j', req.headers);
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(process.env));
 }).listen(process.env.PORT0); // dynamic binding
