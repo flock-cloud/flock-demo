d5ad4fda6f3925231ff3a1edf05c89eea3b7edac
Guido García <guido@tid.es>
2015-03-15 18:13:20 +0100

    anon function

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(function onRequest(req, res) {
     console.log('new request received %j', req.headers);
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(process.env));
